aaaa531833bfab848bc95a5ea68c03a05c561734
Oscar Daniel Otero <ingoscardan@gmail.com>
2023-11-05 02:52:00 -0600

    Installed the dependencies

--- ExpensesControl.API/Program.cs
+++ ExpensesControl.API/Program.cs
@@ -1,4 +1,7 @@
+using System.Configuration;
 using ExpensesControl.API.Services;
+using ExpensesControl.Rdb;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PgSqlDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ExpensesControl")));
 builder.Services.AddScoped<ICreditBucketService, CreditBucketService>();
 builder.Services.AddSwaggerGen();
 
---
+++ ExpensesControl.Rdb/EntityConfigurations/BucketEntityConfiguration.cs
@@ -0,0 +1,13 @@
+using ExpensesControl.Rdb.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpensesControl.Rdb.EntityConfigurations;
+
+public class BucketEntityConfiguration : IEntityTypeConfiguration<BucketEntity>
+{
+    public void Configure(EntityTypeBuilder<BucketEntity> builder)
+    {
+        builder.HasKey(b => b.Id);
+    }
+}

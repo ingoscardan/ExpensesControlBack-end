d9ccc51a0c8c5d153b437998b1f98a238cf5eb0a
Oscar Daniel Otero <ingoscardan@gmail.com>
2023-11-05 15:16:29 -0600

    Add DashedName property to BucketEntity indexed

--- ExpensesControl.Rdb/Entities/BucketEntity.cs
+++ ExpensesControl.Rdb/Entities/BucketEntity.cs
@@ -4,6 +4,7 @@ public class BucketEntity
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public string DashedName { get; set; } = null!;
     public DateTime CreatedDate { get; set; }
     public DateTime LastUpdatedDate { get; set; }
     public decimal Balance { get; set; }
